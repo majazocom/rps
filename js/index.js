c808438c3d876b8fc1844e918c84afb43c3c629c
Maja Gedda <maja.gedda@zocom.se>
2020-10-27 15:47:07 +0100

    final

--- js/index.js
+++ js/index.js
@@ -1,22 +1,41 @@
 let userWeapon; //Här sparar vi valet som användaren gör
 let computerWeapon; //Här sparar vi datorns val
 let test = 'Leffe';
+let userWins = 0;
+let computerWins = 0;
+let rounds = 0;
 
 //Hämta användarens val
 document.getElementById('rock').addEventListener('click', () => {
+    rounds = rounds + 1;
     userWeapon = 'rock';
     randomComputerWeapon();
+    document.getElementById('winner').innerHTML = getWinner();
+    if (rounds == 10) {
+        console.log(userWins, computerWins);
+    }
 })
 document.getElementById('scissor').addEventListener('click', () => {
+    rounds = rounds + 1;
     userWeapon = 'scissor';
     randomComputerWeapon();
+    document.getElementById('winner').innerHTML = getWinner();
+    if (rounds == 10) {
+        console.log(userWins, computerWins);
+    }
 })
 document.getElementById('paper').addEventListener('click', () => {
+    rounds = rounds + 1;
     userWeapon = 'paper';
     randomComputerWeapon();
+    document.getElementById('winner').innerHTML = getWinner();
+    if (rounds == 10) {
+        console.log(userWins, computerWins);
+    }
 })
 //Slumpa datorns val
 let randomComputerWeapon = () => {
+    console.log(rounds);
     computerWeapon = Math.round(Math.random() * 2);
 
     if (computerWeapon == 0) {
@@ -26,24 +45,29 @@ let randomComputerWeapon = () => {
     } else {
         computerWeapon = 'paper'
     }
-    getWinner();
 }
 //Avgör vinnare
 let getWinner = () => {
     if (userWeapon == computerWeapon) {
-        console.log('its a tie');
+        return 'its a tie';
     } else if (userWeapon == 'rock' && computerWeapon == 'scissor') {
-        console.log('A winner is you!');
+        userWins++;
+        return 'you won';
     } else if (userWeapon == 'rock' && computerWeapon == 'paper') {
-        console.log('A winner is computer!');
+        computerWins++;
+        return 'computer won';
     } else if (userWeapon == 'scissor' && computerWeapon == 'paper') {
-        console.log('A winner is you!');
+        userWins++;
+        return 'you won';
     } else if (userWeapon == 'scissor' && computerWeapon == 'rock') {
-        console.log('A winner is computer!');
+        computerWins++;
+        return 'computer won';
     } else if (userWeapon == 'paper' && computerWeapon == 'rock') {
-        console.log('A winner is you!');
+        userWins++;
+        return 'you won';
     } else if (userWeapon == 'paper' && computerWeapon == 'scissor') {
-        console.log('A winner is computer!');
+        computerWins++;
+        return 'computer won';
     }
 }
 
