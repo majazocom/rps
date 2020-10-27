74e93e8ba9b7c1d039f9647baea0862d265314ea
Maja Gedda <maja.gedda@zocom.se>
2020-10-27 13:57:48 +0100

    weapons

--- js/index.js
+++ js/index.js
@@ -3,9 +3,27 @@ let computerWeapon; //Här sparar vi datorns val
 let test = 'Leffe';
 
 //Hämta användarens val
-
+document.getElementById('rock').addEventListener('click', () => {
+    userWeapon = 'rock';
+})
+document.getElementById('scissor').addEventListener('click', () => {
+    userWeapon = 'scissor';
+})
+document.getElementById('paper').addEventListener('click', () => {
+    userWeapon = 'paper';
+})
 //Slumpa datorns val
+let randomComputerWeapon = () => {
+    computerWeapon = Math.round(Math.random() * 2);
 
+    if (computerWeapon == 0) {
+        computerWeapon = 'rock'
+    } else if (computerWeapon == 1) {
+        computerWeapon = 'scissor'
+    } else {
+        computerWeapon = 'paper'
+    }
+}
 //Avgör vinnare
 
 //Visa vinnare
